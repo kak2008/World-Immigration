b5918df96e8a5b59b85cd3a59b84011f25bc95e2
KAK <kak@gmail.com>
2016-11-06 20:36:01 -0500

    login, signup & Forgot password screens finished.

---
+++ World Immigration/World Immigration/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  World Immigration
+//
+//  Created by Vasanth Kodeboyina on 11/6/16.
+//  Copyright © 2016 Anish Kodeboyina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
